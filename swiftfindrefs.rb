e84774ff737d68dedd0deed639407892d80b6011
Michalis Karagiorgos <m.karagiorgos@kaizengaming.com>
2026-01-14 00:51:03 +0200

    bump up version

--- swiftfindrefs.rb
+++ swiftfindrefs.rb
@@ -1,8 +1,8 @@
 class Swiftfindrefs < Formula
   desc "SwiftFindRefs is a macOS Swift CLI that resolves a project’s DerivedData, reads Xcode’s IndexStore, and reports every file referencing a chosen symbol, with optional verbose tracing for diagnostics."
   homepage "https://github.com/michaelversus/SwiftFindRefs"
-  url "https://github.com/michaelversus/SwiftFindRefs.git", tag: "0.1.3"
-  version "0.1.3"
+  url "https://github.com/michaelversus/SwiftFindRefs.git", tag: "0.1.4"
+  version "0.1.4"
 
   depends_on "xcode": [:build]
 

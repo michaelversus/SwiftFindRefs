6d1aa941444c703e200ed6ce200a87ffe30bc26d
Michalis Karagiorgos <m.karagiorgos@kaizengaming.com>
2026-01-14 03:32:57 +0200

    remove md file and bump up version

--- swiftfindrefs.rb
+++ swiftfindrefs.rb
@@ -1,8 +1,8 @@
 class Swiftfindrefs < Formula
   desc "SwiftFindRefs is a macOS Swift CLI that resolves a project’s DerivedData, reads Xcode’s IndexStore, and reports every file referencing a chosen symbol, with optional verbose tracing for diagnostics."
   homepage "https://github.com/michaelversus/SwiftFindRefs"
-  url "https://github.com/michaelversus/SwiftFindRefs.git", tag: "0.2.1"
-  version "0.2.1"
+  url "https://github.com/michaelversus/SwiftFindRefs.git", tag: "0.2.2"
+  version "0.2.2"
 
   depends_on "xcode": [:build]
 
